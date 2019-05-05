44050e958d23c86411c323af23bf0be3ef353ca5
David Mc <my-gitkraken@munkie.biz>
2019-05-05 12:32:42 -0600

    check in current code updates

--- ttms-watchlist-column.ts
+++ ttms-watchlist-column.ts
@@ -1,21 +1,40 @@
 # Squeeze watchlist column
-#     Red Backgrnd - Squeeze is firing in the time period - number indicates how many dots
-#   Green Backgrd - Squeeze has fired up to 5 dots - number indicates how many dots
-#                   and whether Squeeze fired L(ong) or S(hort)
-#   Black Backgrd - No Squeeze in play
+#   Red Background   - Squeeze is building in the time period - number indicates how many dots
+#      w/ white "B"  - Squeeze is building, stock is within "buy zone"
+#   Green Background - Squeeze has fired - number indicates how many dots (up to 5 dots)
+#                      and whether Squeeze fired L(ong) or S(hort)
+#   Black Background - No Squeeze in play
 
 # Original code by Eric Purdy of Simpler Trading 2017
 # Modified code by Rich Stratmann to put in background colors and shorten column values
+# Updates by dmccuskey
+#   - change background colors to aid content scanning
+#   - add "B" to time frames which are in the Buy Zone - between 8 & 21 EMA
+#   - change "0" to " " <space> so that the content doesn't show when row is highlighted
 
-# Shared Link -   http://tos.mx/5cpgKa
+ToS Share Code: http://tos.mx/IPrEhH
+
 def sqz = !TTM_Squeeze().SqueezeAlert;
 def direction = TTM_Squeeze()>TTM_Squeeze()[1];
 def count = if sqz and !sqz[1] then 1 else count[1]+1;
-def fired = if !sqz and sqz[1] then 1 else 0;
-def firedCount = if fired then 1 else firedCount[1]+1;
-def firedDirection = if fired then direction else firedDirection[1];
-# addLabel(yes, if sqz then “Squeeze:” + count else if sum(fired,5) then “Fired:” + firedCOunt + if firedDirection then ” Long” else ” Short” else “-”, if sqz then color.red else if sum(fired,5) and firedDirection then color.green else color.orange);
+def isFired = if !sqz and sqz[1] then 1 else 0;
+def firedCount = if isFired then 1 else firedCount[1]+1;
+def firedDirection = if isFired then direction else firedDirection[1];
 
-addLabel(yes, if sqz then "" + count else if sum(fired,5) then “” + firedCOunt + if firedDirection then ” L” else ” S” else “” + 0, if sqz then color.white else if sum(fired,5) and firedDirection then color.black else color.black);
+def sumIsFired = sum(isFired,5);
+def isFiredDir = sumIsFired && firedDirection;
 
-AssignBackgroundColor(if sqz then color.red else if sum(fired,5)then color.green else color.black);
+# look for close buy zone
+def ema8 = reference movAvgExponential(length=8);
+def ema21 = reference movAvgExponential(length=21);
+def currPrice = close();
+def highVal = Max(ema8, ema21);
+def lowVal = Min(ema8, ema21);
+def inBuyZone = currPrice >= lowVal && currPrice <= highVal;
+
+def sqzBuy = sqz && inBuyZone;
+def sqzNoBuy = sqz && !inBuyZone;
+
+addLabel(yes, Concat(if sqzBuy then "B " else "", if sqz then "" + count else if sumIsFired then “” + firedCount + if firedDirection then ” L” else ” S” else “ ”), if sqzBuy then color.white else color.black);
+
+AssignBackgroundColor(if sqzNoBuy then  CreateColor(170, 6, 0) else if sqzBuy then color.red else if sumIsFired then CreateColor(28, 105, 3) else color.black);
